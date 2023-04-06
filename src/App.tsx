1c5263ebff462e0543d0cf5d4d77508ad399940d
MikolajBlaszczyk <101048261+MikolajBlaszczyk@users.noreply.github.com>
2023-04-06 09:03:44 +0200

    changes to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Card from './components/card/card'
+
+const TAP_CHAMPION_DESCRIPTION = 'My Github with source code files Introduction Tap Champion is my clone of “Guitar Hero” mixed with “Osu!” and “Genshin Impact”s event-mini game.The project is designed and coded entirely by myself. I want to focus on usability andthe backend side of it.In the future I plan to work on the general appearance and aesthetics (UI,… '
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="AppHeader">
+          <h1>Site contains information about my game projects</h1>
+      </div>
+      <div id="AppContent">
+        <Card 
+          imageSource='/images/menu.png'
+          title='Tap Champion'
+          description={TAP_CHAMPION_DESCRIPTION}
+          />
+
+      <Card 
+          imageSource='/images/veggies.png'
+          title='Tap Champion'
+          description={TAP_CHAMPION_DESCRIPTION}
+          />
+      </div>
     </div>
   );
 }
